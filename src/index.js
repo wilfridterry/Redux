75ec8bffd66934e2f61c0508baf1837daaa5daf3
Stannis <zalevschi1998@gmail.com>
2022-08-12 00:27:21 +0300

    Started learning redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { legacy_createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const initialState = { value: 0 };
+
+const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "INC":
+      return {
+        ...state,
+        value: state.value + 1,
+      };
+    case "DEC":
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    case "RND":
+      return {
+        ...state,
+        value: state.value * payload,
+      };
+    default:
+      return {...state};
+  }
+};
+
+let store = legacy_createStore(reducer);
+
+const update = () => {
+  console.log(store.getState());
+  document.getElementById("counter").innerHTML = store.getState().value;
+  document.getElementById("custom").innerHTML = store.getState().value;
+
+};
+
+store.subscribe(update);
+
+const inc = () => ({ type: "INC" });
+const dec = () => ({ type: "DEC" });
+const rnd = (value) => ({ type: "RND", payload: value });
+
+document.getElementById("inc").addEventListener("click", () => {
+  store.dispatch(inc());
+});
+
+document.getElementById("dec").addEventListener("click", () => {
+  store.dispatch(dec());
+});
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+
+  store.dispatch(rnd(value));
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <></>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
