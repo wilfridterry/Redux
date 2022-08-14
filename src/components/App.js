987915bbdd42fc3f90e01f248a76501ba6b12c58
Stannis <zalevschi1998@gmail.com>
2022-08-14 21:43:57 +0300

    Added react-redux to the project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import Counter from "./Counter";
+
+const App = () => {
+    return (
+        <Counter
+        // counter={getState().value}
+        // inc={inc}
+        // dec={dec}
+        // rnd={() => {
+        //   const value = Math.floor(Math.random() * 10);
+
+        //   rnd(value);
+        // }}
+      />
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore, bindActionCreators } from "redux";
-
+import { legacy_createStore } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
+import App from "./components/App";
+import { Provider } from "react-redux";
 
-let { dispatch, subscribe, getState } = legacy_createStore(reducer);
-
-const update = () => {
-  console.log(getState());
-  document.getElementById("counter").innerHTML = getState().value;
-  document.getElementById("custom").innerHTML = getState().value;
-};
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args))
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-// const decDispatch = bindActionCreators(dec, dispatch);
-
-// const rndDispatch = bindActionCreators(rnd, dispatch);
-
-document.getElementById("inc").addEventListener("click", inc);
-
-document.getElementById("dec").addEventListener("click", dec);
-
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-
-  rnd(value);
-});
+const store = legacy_createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <></>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
