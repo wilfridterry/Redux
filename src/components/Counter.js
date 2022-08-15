4362e9a724dc52cdee606df2eb4a03d0aaf5cdb5
Stannis <zalevschi1998@gmail.com>
2022-08-15 23:49:02 +0300

    Added a setting for Redux devtools

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,27 @@
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux/es/exports";
-import * as actions from "../actions";
+import {inc, dec, rnd} from "../actions";
 
 const Counter = () => {
-
   const counter = useSelector(state => {
     return state.counter;
   });
   
   const dispatch = useDispatch();
-
+  const memoizedInc = useCallback(() => dispatch(inc()), []);
+  const memoizedDec = useCallback(() => dispatch(dec()), []);
+  const memoizedRnd = useCallback(() => dispatch(rnd()), []);
+  
   return (
     <div className="jumbotron">
       <h1>{counter}</h1>
-      <button onClick={() => dispatch(actions.dec)} className="btn btn-primary">
+      <button onClick={memoizedDec} className="btn btn-primary">
         DEC
       </button>
-      <button onClick={() => dispatch(actions.dec)} className="btn btn-primary">
+      <button onClick={memoizedInc} className="btn btn-primary">
         INC
       </button>
-      <button onClick={() => dispatch(actions.rnd)} className="btn btn-primary">
+      <button onClick={memoizedRnd} className="btn btn-primary">
         RND
       </button>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import reducer from "./reducer";
 import App from "./components/App";
 import { Provider } from "react-redux";
 
-const store = legacy_createStore(reducer);
+const store = legacy_createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
